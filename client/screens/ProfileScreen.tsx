30a6dc164339754a29b67baaafda3e2119c7bf34
gdavis <gregory.davis@autozone.com>
2022-08-23 15:48:25 -0400

    feat(valet): feature for customer to request their car returned

--- client/screens/ProfileScreen.tsx
+++ client/screens/ProfileScreen.tsx
@@ -1,14 +1,29 @@
-import { StyleSheet } from 'react-native';
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { FontAwesome } from "@expo/vector-icons";
+import { Linking, Modal, StyleSheet, Pressable, Platform } from "react-native";
+import { Text, View } from "../components/Themed";
+import { RootTabScreenProps, RootState } from "../types";
+import { logoutUser } from "../redux/actions/authActions/logoutUser";
+import dayjs from "dayjs";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
+export default function ProfileScreen({
+  navigation,
+}: RootTabScreenProps<"ProfileScreen">) {
+  const dispatch = useDispatch();
+  const userFirstName = useSelector(
+    (state: RootState) => state.user.userFirstName,
+  );
 
-export default function ProfileScreen() {
+  //   const handleLogOut = () => {
+  //     dispatch(logoutUser());
+  //     setModalVisible(!modalVisible);
+  //   };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabTwoScreen.tsx" />
+      <Text>PROFILE HERE</Text>
     </View>
   );
 }
@@ -16,16 +31,94 @@ export default function ProfileScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "flex-start",
+    padding: 16,
   },
+  viewProfileText: {
+    color: "#63666A",
+    paddingTop: 8,
+  },
+  greeting: { fontSize: 24 },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "100%",
+  },
+  profileSubSection: {},
+  profileSubSectionTitle: {
+    fontSize: 20,
+  },
+  contactOptionsContainer: {
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    padding: 16,
+  },
+  contactOption: {
+    padding: 36,
+    borderRadius: 12,
+    borderStartColor: "red",
+    // elevation: 2,
+  },
+  // BUTTON CALL/TEXT
+  optionButton: {
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 10,
+    backgroundColor: "black",
+  },
+  text: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: "bold",
+    letterSpacing: 0.25,
+    color: "white",
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 12,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonOpen: {
+    backgroundColor: "#F194FF",
+  },
+  buttonClose: {
+    backgroundColor: "#2196F3",
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center",
   },
 });
