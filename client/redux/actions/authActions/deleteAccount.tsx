9c2ce510ad2b10952ca2c657418fbdbb26efec58
gdavis <gregory.davis@autozone.com>
2022-09-09 00:59:27 -0400

    fix(jwt): remove jwt on delete account

--- client/redux/actions/authActions/deleteAccount.tsx
+++ client/redux/actions/authActions/deleteAccount.tsx
@@ -1,28 +1,39 @@
 import axios from "../../../utils/axios";
+import * as SecureStore from "expo-secure-store";
 
 export const deleteAccount = () => {
-  return (dispatch: (arg0: {type: string; userAuthenticated?: any; message?: any;}) => void) => {
-    const request = axios.post("/auth/deleteAccount");
-    request
-      .then((request) => {
-        const { data } = request;
-        dispatch({
-          type: "auth/deleteAccount",
-          userAuthenticated: data.userAuthenticated,
-          message: data.message
-        });
-        dispatch({
-          type: "service/clearServiceData",
-        });
-        dispatch({
-          type: "service/clearReturnServiceData",
-        });
-        dispatch({
-          type: "user/clearUserData",
-        });
-      })
-      .catch((err) => {
-        console.error("userSignIn fn() catch block in the network call:", err);
+  return async (
+    dispatch: (arg0: {
+      type: string;
+      userAuthenticated?: any;
+      message?: any;
+    }) => void,
+  ) => {
+    try {
+      const request = await axios.post("/auth/deleteAccount");
+
+      const { data } = request;
+      dispatch({
+        type: "auth/deleteAccount",
+        userAuthenticated: data.userAuthenticated,
+        message: data.message,
       });
+      dispatch({
+        type: "service/clearServiceData",
+      });
+      dispatch({
+        type: "service/clearReturnServiceData",
+      });
+      dispatch({
+        type: "user/clearUserData",
+      });
+      // remove jwt token from secure store
+      await SecureStore.deleteItemAsync("accessToken");
+    } catch (error: any) {
+      console.error(
+        "userSignIn fn() catch block in the network call:",
+        error.response.data,
+      );
+    }
   };
 };
